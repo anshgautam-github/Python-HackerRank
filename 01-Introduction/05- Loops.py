e0fbf5d819a146bed039e4b4283b8e2e8413d800
Ansh Gautam <95379812+anshgautam-github@users.noreply.github.com>
2026-06-17 17:07:38 +0530

    Update 05- Loops.py

--- 01-Introduction/05- Loops.py
+++ 01-Introduction/05- Loops.py
@@ -1,4 +1,7 @@
+# Task
+# The provided code stub reads an integer, , from STDIN. For all non-negative integers i<n, print i**2.
+
 if __name__ == '__main__':
     n = int(input())
-    for i in range(0,n,1):
+    for i in range(n):
         print(i**2);
