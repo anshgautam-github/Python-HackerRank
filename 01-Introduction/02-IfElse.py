f141023c18e6bba8887226e030ee11f34959c632
Ansh Gautam <95379812+anshgautam-github@users.noreply.github.com>
2026-06-17 17:01:51 +0530

    Update 02-IfElse.py

--- 01-Introduction/02-IfElse.py
+++ 01-Introduction/02-IfElse.py
@@ -18,14 +18,13 @@ import re
 import sys
 
 
-
 if __name__ == '__main__':
     n = int(input().strip())
-    if n%2!=0:
-        print("Weird")
-    elif n<=5 and n>=2:
-        print("Not Weird")
-    elif n<=20 and n>=6:
-        print("Weird")
-    else:
-        print("Not Weird")
+    if (n%2!=0):
+         print("Weird")
+    elif (n%2==0 and 2<=n<=5):
+         print("Not Weird")
+    elif (n%2==0 and 6<=n<=20):
+         print("Weird")
+    elif (n%2==0 and n>20):
+         print("Not Weird")
