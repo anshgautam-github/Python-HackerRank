813c143a6d6f3d0f35f0778f0f35db9f6b54b4b2
Ansh Gautam <95379812+anshgautam-github@users.noreply.github.com>
2026-06-17 17:11:47 +0530

    Update 06- Write A Function.py

--- 01-Introduction/06- Write A Function.py
+++ 01-Introduction/06- Write A Function.py
@@ -2,6 +2,11 @@
 # Note that the code stub provided reads from STDIN and passes arguments to the is_leap function. 
 # It is only necessary to complete the is_leap function.
 
+# In the Gregorian calendar, three conditions are used to identify leap years:
+# The year can be evenly divided by 4, is a leap year, unless:
+# The year can be evenly divided by 100, it is NOT a leap year, unless:
+# The year is also evenly divisible by 400. Then it is a leap year.
+
 def is_leap(year):
     leap = False
     
