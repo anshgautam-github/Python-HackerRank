872c76aa1de71a3c2949f5fb93fcb991e001ee54
Ansh Gautam <95379812+anshgautam-github@users.noreply.github.com>
2026-06-18 00:37:41 +0530

    Create 03-What's Your Name?.py

---
+++ 03-Strings/03-What's Your Name?.py
@@ -0,0 +1,12 @@
+# Hello firstname lastname! You just delved into python.
+
+# Function Description
+# Complete the print_full_name function in the editor below.
+
+# print_full_name has the following parameters:
+# string first: the first name
+# string last: the last name
+
+def print_full_name(first, last):
+    # Write your code here
+    print(f"Hello {first} {last}! You just delved into python.")
