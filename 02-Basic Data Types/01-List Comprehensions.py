3c0fd05452db13f7e43a9de67332f1b22da52744
Ansh Gautam <95379812+anshgautam-github@users.noreply.github.com>
2026-06-21 12:31:05 +0530

    Update 01-List Comprehensions.py

--- 02-Basic Data Types/01-List Comprehensions.py
+++ 02-Basic Data Types/01-List Comprehensions.py
@@ -13,3 +13,16 @@ result = [[i,j,k]
           if i+j+k != n]
 
 print(result)
+
+
+
+# NORMALLY without list comprehension :
+# result = []
+# for i in range(x+1):
+#     for j in range(y+1):
+#         for k in range(z+1):
+
+#             if i+j+k != n:
+#                 result.append([i,j,k])
+
+# print(result)
