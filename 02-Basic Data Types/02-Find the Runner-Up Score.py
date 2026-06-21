01af569ccbe75608be047d0da7ae30878343db00
Ansh Gautam <95379812+anshgautam-github@users.noreply.github.com>
2026-06-21 12:54:40 +0530

    Update 02-Find the Runner-Up Score.py

--- 02-Basic Data Types/02-Find the Runner-Up Score.py
+++ 02-Basic Data Types/02-Find the Runner-Up Score.py
@@ -1,1 +1,8 @@
+# Given the participants' score sheet for your University Sports Day, you are required to find the runner-up score. 
+# You are given n scores. Store them in a list and find the score of the runner-up.
 
+n = int(input())
+arr = list(map(int, input().split()))
+print(sorted(set(arr))[-2])
+
+# Here the split usually gives the list of strings, but we want integers so map converts them in integer 
